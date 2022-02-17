11b35197616afac9a7bb027f59429c3514ddfce5
Wexengos <gnos2007@gmail.com>
2022-02-16 23:21:38 -0300

    Pesquisa + filtros no select

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,35 +1,50 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
+
+import Select from "react-select";
 
 import styles from "./styles.module.scss";
 
 import { FilterContext } from "../../App";
-
-function handleSearch(filters, setFilters, event) {
-  // setIsSearched(true);
-  let value = event.target.value.toLowerCase();
-  let result = [];
-
-  console.log("value: ", value);
-
-  result = filters.filter((data) => {
-    let exists =
-      data.meta.title.toString().toLowerCase().search(value) !== -1;
-    return exists;
-  });
-
-  setFilters(result);
-}
+import { options } from "./filterOptions";
 
 function Header() {
-  const { filters, setFilters } = useContext(FilterContext);
+  const { data, setFilteredData } = useContext(FilterContext);
+
+  const [currentFilter, setCurrentFilter] = useState();
+
+  function handleSearch(data, setFilteredData, event) {
+    // setIsSearched(true);
+    let value = event.target.value.toLowerCase();
+    let result = [];
+
+    console.log("value: ", value);
+    console.log("data: ", data);
+
+    result = data.filter((target) => {
+      if (target[currentFilter] !== null) {
+        let exists =
+          target[currentFilter].toString().toLowerCase().search(value) !== -1;
+        return exists;
+      }
+    });
+
+    setFilteredData(result);
+  }
 
   return (
     <div className={styles.headerContainer}>
       <div className={styles.headerContent}>
+        <Select
+          options={options}
+          onChange={(selectedOption) => {
+            console.log("filtro: ", currentFilter);
+            setCurrentFilter(selectedOption.value);
+          }}
+        />
         <input
           type="text"
           className={styles.searchInput}
-          onChange={(e) => handleSearch(filters, setFilters, e)}
+          onChange={(e) => handleSearch(data, setFilteredData, e)}
         />
       </div>
     </div>
